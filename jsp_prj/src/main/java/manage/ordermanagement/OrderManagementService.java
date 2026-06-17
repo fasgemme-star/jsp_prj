3ae09cddef272722b6a4e73ddbadecb18e537806
gst05 <gst05@112.169.242.37>
2026-06-17 20:49:47 +0900

    0617

--- jsp_prj/src/main/java/manage/ordermanagement/OrderManagementService.java
+++ jsp_prj/src/main/java/manage/ordermanagement/OrderManagementService.java
@@ -1,5 +1,6 @@
 package manage.ordermanagement;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderManagementService {
@@ -26,59 +27,61 @@ public class OrderManagementService {
 	}// endNum
 	
 	public List<OrderDTO> getOrderList(){
-		
-	}
+		List<OrderDTO> oList = new ArrayList<OrderDTO>();
+		return oList;
+	}// getOrderList
 	
 	public OrderDTO getOrderDetail(String orderID) {
-		
-	}
+		OrderDTO oDTO = new OrderDTO();
+		return oDTO;
+	}// getOrderDetail
 	
 	public List<OrderDTO> searchOrderByClient(String clientID){
-		
-	}
+		List<OrderDTO> oList = new ArrayList<OrderDTO>();
+		return oList;
+	}// searchOrderByClient
 	
 	public List<OrderDTO> searchOrderByStatus(String status){
-		
-	}
+		List<OrderDTO> oList = new ArrayList<OrderDTO>();
+		return oList;
+	}// searchOrderByStatus
 	
 	public List<OrderDTO> searchOrderByDate(String startDate, String endDate){
-		
-	}
+		List<OrderDTO> oList = new ArrayList<OrderDTO>();
+		return oList;
+	}// searchOrderByDate
 	
-	public 
-	public 
-	public 
-	public 
-	public 
-	public 
-	public 
-	public 
-	public 
+	public boolean processDelivery(int orderID) {
+		return true;
+	}// processDelivery
 	
+	public ClaimDTO getClaimDetail(String claimID) {
+		ClaimDTO cDTO = new ClaimDTO();
+		return cDTO;
+	}// getClaimDetail
 	
+	public boolean approveCancle(String claimID) {
+		return true;
+	}// approveCancle
 	
+	public boolean rejectCancel(String claimID) {
+		return true;
+	}// rejectCancel
 	
+	public boolean approveExchange(String claimID) {
+		return true;
+	}// approveExchange
 	
+	public boolean rejectExchange(String claimID) {
+		return true;
+	}// rejectExchange
 	
+	public boolean approveReturn(String claimID) {
+		return true;
+	}// approveReturn
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public boolean rejectReturn(String claimID) {
+		return true;
+	}// rejectReturn
+
 }
